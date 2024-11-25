d7f0789cc8695567d93fbed24664fa701112079d
dhairyabhatt946 <dhairyabhatt946@gmail.com>
2024-11-25 18:50:39 +0530

    Chapter 3

---
+++ Chapter 3/02_negative_slicing.py
@@ -0,0 +1,13 @@
+name = "Harry"
+
+print(name[0:3])
+
+print(name[-4:-1])
+print(name[1:4])
+
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:5])
+print(name[1:5])
+
+word = "amazing"
+print(word[1:6:2]) # mzn
---
+++ Chapter 3/03_str_functions.py
@@ -0,0 +1,6 @@
+name = "Harry noob"
+
+print(len(name))
+print(name.endswith("rry"))
+print(name.startswith("ha"))
+print(name.capitalize()) # capitalizes only starting character which is at index 0
